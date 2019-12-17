7b58eb87090e3371c23e98122ec1b9e587362162
Arthur Siqueira <arthur.siq5@gmail.com>
2019-12-17 14:54:56 -0300

    criado código de controle do formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-reactive-form-test';
+export class AppComponent implements OnInit{
+  
+  formulario: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){ }
+
+  ngOnInit(){
+    this.criarFormulario();
+  }
+
+  enviarDados(){
+    console.log(this.formulario.value);
+  }
+
+  criarFormulario() {
+    this.formulario = this.formBuilder.group({
+      nome: [''],
+      email: [''],
+      cpf: [''],
+      nascimento: [''],
+      senha: [''],
+      confirmarSenha: ['']
+    });
+  }
 }
